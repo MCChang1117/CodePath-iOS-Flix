dde12265377b215a735186a0bb229c0547d06d21
MCChang macbook <meng.chuan.chang@gmail.com>
2021-09-23 00:01:54 -0400

    Step 3: Create a Network Request

---
+++ CodePath-flixster-01Tests/CodePath_flixster_01Tests.swift
@@ -0,0 +1,33 @@
+//
+//  CodePath_flixster_01Tests.swift
+//  CodePath-flixster-01Tests
+//
+//  Created by Meng-Chuan Chang on 9/20/21.
+//
+
+import XCTest
+@testable import CodePath_flixster_01
+
+class CodePath_flixster_01Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
