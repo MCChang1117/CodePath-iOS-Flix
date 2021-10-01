fa9a8ae375b6a8bc8f2e04218dcd0b7347ecf253
MCChang macbook <meng.chuan.chang@gmail.com>
2021-09-30 23:51:30 -0400

    Step 2: Collection View Setup

---
+++ CodePath-flixster-01/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  CodePath-flixster-01
+//
+//  Created by Meng-Chuan Chang on 9/30/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
